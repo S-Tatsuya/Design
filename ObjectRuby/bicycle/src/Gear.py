b244917dcc1a7514d068a6ee83af71cd5321cc72
S.tatsuya <s.tatsuya0123@gmail.com>
2022-12-29 16:27:50 +0900

    refactor: GearからWheelへの依存を無くす

--- ObjectRuby/bicycle/src/Gear.py
+++ ObjectRuby/bicycle/src/Gear.py
@@ -1,19 +1,11 @@
-from src.Wheel import Wheel
-
-
 class Gear:
-    def __init__(self, chainring, cog, wheel=Wheel(0, 0)):
+    def __init__(self, chainring, cog):
         self.__chainring = chainring
         self.__cog = cog
-        self.__wheel = wheel
 
     @property
     def Ratio(self):
         return self.__chainring / self.__cog
 
-    @property
-    def GearInches(self):
-        return self.Ratio * self.__diameter()
-
-    def __diameter(self):
-        return self.__wheel.Diameter
+    def gearInches(self, diameter):
+        return self.Ratio * diameter
--- ObjectRuby/bicycle/tests/test_bicycle.py
+++ ObjectRuby/bicycle/tests/test_bicycle.py
@@ -4,17 +4,17 @@ import math
 
 
 def test_gear():
-    gear = Gear(52, 11, Wheel(26, 1.5))
+    gear = Gear(52, 11)
     result = 52 / 11
     assert math.isclose(result, gear.Ratio)
 
     result = (52 / 11) * (26 + (1.5 * 2))
-    assert math.isclose(result, gear.GearInches)
+    assert math.isclose(result, gear.gearInches(Wheel(26, 1.5).Diameter))
 
     gear = Gear(52, 11)
     result = 52 / 11
     assert math.isclose(result, gear.Ratio)
-    assert math.isclose(0, gear.GearInches)
+    assert math.isclose(0, gear.gearInches(Wheel(0, 0).Diameter))
 
 
 def test_wheel():
