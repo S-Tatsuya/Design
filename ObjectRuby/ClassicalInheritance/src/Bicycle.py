d7a0ae256c39facc22bf3e765152d9a1b239288f
S.tatsuya <s.tatsuya0123@gmail.com>
2023-01-03 09:47:07 +0900

    Revert "mountainの場合をベタ書きで追加（悪い例）"
    This reverts commit 34829e1dc31c6aabe4d01cbd5ae7756068c9270c.

--- ObjectRuby/ClassicalInheritance/src/Bicycle.py
+++ ObjectRuby/ClassicalInheritance/src/Bicycle.py
@@ -1,10 +1,7 @@
 class Bicycle:
     def __init__(self, **kwargs):
-        self._style = kwargs["style"] if "style" in kwargs.keys() else "road"
-        self._size = kwargs.get("size")
-        self._tape_color = kwargs.get("tape_color")
-        self._front_shock = kwargs.get("front_shock")
-        self._rear_shock = kwargs.get("rear_shock")
+        self._size = kwargs["size"]
+        self._tape_color = kwargs["tape_color"]
 
     @property
     def size(self):
@@ -12,15 +9,4 @@ class Bicycle:
 
     @property
     def spares(self):
-        if self._style == "road":
-            return {
-                "chain": "10-speed",
-                "tire_size": "23",
-                "tape_color": self._tape_color,
-            }
-        else:
-            return {
-                "chain": "10-speed",
-                "tire_size": "2.1",
-                "rear_shock": self._rear_shock,
-            }
+        return {"chain": "10-speed", "tire_size": "23", "tape_color": self._tape_color}
--- ObjectRuby/ClassicalInheritance/tests/test_bicycle.py
+++ ObjectRuby/ClassicalInheritance/tests/test_bicycle.py
@@ -1,14 +1,8 @@
 from src.Bicycle import Bicycle
 
 
-def test_bicycle_road():
+def test_bicycle():
     sut = Bicycle(size="M", tape_color="red")
 
     assert sut.size == "M"
     assert sut.spares == {"tire_size": "23", "chain": "10-speed", "tape_color": "red"}
-
-
-def test_bicycle_mountain():
-    sut = Bicycle(style="mountain", size="S", front_shock="Manitou", rear_shock="Fox")
-
-    assert sut.spares == {"tire_size": "2.1", "chain": "10-speed", "rear_shock": "Fox"}
