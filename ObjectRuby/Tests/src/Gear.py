ba5dd5b06212877547b41f4d5f54372dd1764e9b
S.tatsuya <s.tatsuya0123@gmail.com>
2023-01-10 20:59:01 +0900

    9-4章を対応中

--- ObjectRuby/Tests/src/Gear.py
+++ ObjectRuby/Tests/src/Gear.py
@@ -6,10 +6,32 @@ class Gear:
         self._chainring = kwargs.get("chainring") or 0
         self._cog = kwargs.get("cog") or 0
         self._wheel = kwargs.get("wheel") or Wheel(0, 0)
+        self._observer = kwargs.get("observer")
 
     def gear_inches(self):
         return self.ratio * self._wheel.diameter()
 
     @property
     def ratio(self):
-        return self._chainring / self._cog
+        return self.chainring / self.cog
+
+    @property
+    def cog(self):
+        return self._cog
+
+    @cog.setter
+    def cog(self, new_cog):
+        self._cog = new_cog
+        self.changed()
+
+    @property
+    def chainring(self):
+        return self._chainring
+
+    @chainring.setter
+    def chainring(self, new_chainring):
+        self._chainring = new_chainring
+        self.changed()
+
+    def changed(self):
+        self._observer.changed(self.chainring, self.cog)
--- ObjectRuby/Tests/tests/test_gear.py
+++ ObjectRuby/Tests/tests/test_gear.py
@@ -1,10 +1,14 @@
 import pytest
 
 from src.Gear import Gear
-from src.Wheel import Wheel
+
+
+class DiameterDouble:
+    def diameter(self):
+        return 10
 
 
 def test_calculates_gear_inches():
-    sut = Gear(chainring=52, cog=11, wheel=Wheel(26, 1.5))
+    sut = Gear(chainring=52, cog=11, wheel=DiameterDouble())
 
-    assert pytest.approx(sut.gear_inches(), 0.1) == 137.1
+    assert pytest.approx(sut.gear_inches(), 0.01) == 47.27
