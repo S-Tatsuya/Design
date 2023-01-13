26dbc7ef6cdc0bd20511528a06dc259943fb79fc
S.tatsuya <s.tatsuya0123@gmail.com>
2023-01-13 18:23:17 +0900

    add: for文による繰り返し制御を追加

--- Algorithm/MeikaiPython/Chapter1/src/chapter1_2.py
+++ Algorithm/MeikaiPython/Chapter1/src/chapter1_2.py
@@ -9,3 +9,14 @@ def sum1ton_while():
         i += 1
 
     print(f"1から{n}までの総和は{sum}です。")
+
+
+def sum1ton_for():
+    print("1からnまでの総和を求めます。")
+    n = int(input("nの値:"))
+
+    sum = 0
+    for i in range(1, n + 1):
+        sum += i
+
+    print(f"1から{n}までの総和は{sum}です。")
--- Algorithm/MeikaiPython/Chapter1/tests/test_chapter1_2.py
+++ Algorithm/MeikaiPython/Chapter1/tests/test_chapter1_2.py
@@ -11,3 +11,11 @@ class TestChapter1_2:
 
         assert out == "1からnまでの総和を求めます。\nnの値:1から5までの総和は15です。\n"
         assert err == ""
+
+    def test_sum1ton_while(self, monkeypatch, capfd):
+        monkeypatch.setattr("sys.stdin", io.StringIO("5\n"))
+        sut.sum1ton_for()
+        out, err = capfd.readouterr()
+
+        assert out == "1からnまでの総和を求めます。\nnの値:1から5までの総和は15です。\n"
+        assert err == ""
