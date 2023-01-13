a3b59b1ea8656172a4fbf70e371e4f924bfa62d2
S.tatsuya <s.tatsuya0123@gmail.com>
2023-01-13 18:10:48 +0900

    refactor: ファイル名を変更

--- Algorithm/MeikaiPython/Chapter1/tests/test_chapter1_1.py
+++ Algorithm/MeikaiPython/Chapter1/tests/test_chapter1_1.py
@@ -1,51 +1,51 @@
 import io
 
-import src.chapter1 as chapter1
+import src.chapter1_1 as chapter1_1
 
 
-class TestChapter1:
+class Testchapter1_1:
     def test_max3(self, monkeypatch, capfd):
         monkeypatch.setattr("sys.stdin", io.StringIO("1\n2\n3"))
 
-        chapter1.max3()
+        chapter1_1.max3()
 
         out, err = capfd.readouterr()
         assert out == "三つの整数の最大値を求めます。\n整数aの値:整数bの値:整数cの値:最大値は3です。\n"
         assert err == ""
 
     def test_max3_func(self):
-        assert chapter1.max3_func(3, 2, 1) == 3
-        assert chapter1.max3_func(3, 2, 2) == 3
-        assert chapter1.max3_func(3, 1, 2) == 3
-        assert chapter1.max3_func(3, 2, 3) == 3
-        assert chapter1.max3_func(2, 1, 3) == 3
-        assert chapter1.max3_func(3, 3, 2) == 3
-        assert chapter1.max3_func(3, 3, 3) == 3
-        assert chapter1.max3_func(2, 2, 3) == 3
-        assert chapter1.max3_func(2, 3, 1) == 3
-        assert chapter1.max3_func(2, 3, 2) == 3
-        assert chapter1.max3_func(1, 3, 2) == 3
-        assert chapter1.max3_func(2, 3, 3) == 3
-        assert chapter1.max3_func(1, 2, 3) == 3
+        assert chapter1_1.max3_func(3, 2, 1) == 3
+        assert chapter1_1.max3_func(3, 2, 2) == 3
+        assert chapter1_1.max3_func(3, 1, 2) == 3
+        assert chapter1_1.max3_func(3, 2, 3) == 3
+        assert chapter1_1.max3_func(2, 1, 3) == 3
+        assert chapter1_1.max3_func(3, 3, 2) == 3
+        assert chapter1_1.max3_func(3, 3, 3) == 3
+        assert chapter1_1.max3_func(2, 2, 3) == 3
+        assert chapter1_1.max3_func(2, 3, 1) == 3
+        assert chapter1_1.max3_func(2, 3, 2) == 3
+        assert chapter1_1.max3_func(1, 3, 2) == 3
+        assert chapter1_1.max3_func(2, 3, 3) == 3
+        assert chapter1_1.max3_func(1, 2, 3) == 3
 
     def test_med3_func(self):
-        assert chapter1.med3_func(3, 2, 1) == 2
-        assert chapter1.med3_func(3, 2, 2) == 2
-        assert chapter1.med3_func(3, 1, 2) == 2
-        assert chapter1.med3_func(3, 2, 3) == 3
-        assert chapter1.med3_func(2, 1, 3) == 2
-        assert chapter1.med3_func(3, 3, 2) == 3
-        assert chapter1.med3_func(3, 3, 3) == 3
-        assert chapter1.med3_func(2, 2, 3) == 2
-        assert chapter1.med3_func(2, 3, 1) == 2
-        assert chapter1.med3_func(2, 3, 2) == 2
-        assert chapter1.med3_func(1, 3, 2) == 2
-        assert chapter1.med3_func(2, 3, 3) == 3
-        assert chapter1.med3_func(1, 2, 3) == 2
+        assert chapter1_1.med3_func(3, 2, 1) == 2
+        assert chapter1_1.med3_func(3, 2, 2) == 2
+        assert chapter1_1.med3_func(3, 1, 2) == 2
+        assert chapter1_1.med3_func(3, 2, 3) == 3
+        assert chapter1_1.med3_func(2, 1, 3) == 2
+        assert chapter1_1.med3_func(3, 3, 2) == 3
+        assert chapter1_1.med3_func(3, 3, 3) == 3
+        assert chapter1_1.med3_func(2, 2, 3) == 2
+        assert chapter1_1.med3_func(2, 3, 1) == 2
+        assert chapter1_1.med3_func(2, 3, 2) == 2
+        assert chapter1_1.med3_func(1, 3, 2) == 2
+        assert chapter1_1.med3_func(2, 3, 3) == 3
+        assert chapter1_1.med3_func(1, 2, 3) == 2
 
     def test_judge_sign(self, monkeypatch, capfd):
         monkeypatch.setattr("sys.stdin", io.StringIO("17\n"))
-        chapter1.judge_sign()
+        chapter1_1.judge_sign()
 
         out, err = capfd.readouterr()
 
@@ -53,7 +53,7 @@ class TestChapter1:
         assert err == ""
 
         monkeypatch.setattr("sys.stdin", io.StringIO("-5\n"))
-        chapter1.judge_sign()
+        chapter1_1.judge_sign()
 
         out, err = capfd.readouterr()
 
@@ -61,7 +61,7 @@ class TestChapter1:
         assert err == ""
 
         monkeypatch.setattr("sys.stdin", io.StringIO("0\n"))
-        chapter1.judge_sign()
+        chapter1_1.judge_sign()
 
         out, err = capfd.readouterr()
 
@@ -70,25 +70,25 @@ class TestChapter1:
 
     def test_branch(self, monkeypatch, capfd):
         monkeypatch.setattr("sys.stdin", io.StringIO("3\n"))
-        chapter1.branch1()
+        chapter1_1.branch1()
         out, err = capfd.readouterr()
         assert out == "整数:C\n"
         assert err == ""
 
         monkeypatch.setattr("sys.stdin", io.StringIO("3\n"))
-        chapter1.branch2()
+        chapter1_1.branch2()
         out, err = capfd.readouterr()
         assert out == "整数:C\n"
         assert err == ""
 
         monkeypatch.setattr("sys.stdin", io.StringIO("4\n"))
-        chapter1.branch1()
+        chapter1_1.branch1()
         out, err = capfd.readouterr()
         assert out == "整数:C\n"
         assert err == ""
 
         monkeypatch.setattr("sys.stdin", io.StringIO("4\n"))
-        chapter1.branch2()
+        chapter1_1.branch2()
         out, err = capfd.readouterr()
         assert out == "整数:\n"
         assert err == ""
