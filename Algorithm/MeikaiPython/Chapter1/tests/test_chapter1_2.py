8ffbbff9f52b09d35f16114c7daea18dc87811ca
S.tatsuya <s.tatsuya0123@gmail.com>
2023-01-13 18:18:35 +0900

    add: whileループを使った制御を追加

---
+++ Algorithm/MeikaiPython/Chapter1/tests/test_chapter1_2.py
@@ -0,0 +1,13 @@
+import io
+
+import src.chapter1_2 as sut
+
+
+class TestChapter1_2:
+    def test_sum1ton_while(self, monkeypatch, capfd):
+        monkeypatch.setattr("sys.stdin", io.StringIO("5\n"))
+        sut.sum1ton_while()
+        out, err = capfd.readouterr()
+
+        assert out == "1からnまでの総和を求めます。\nnの値:1から5までの総和は15です。\n"
+        assert err == ""
