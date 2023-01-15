61acbf8cfb024586561629953aa56a95b9edf299
S.tatsuya <s.tatsuya0123@gmail.com>
2023-01-15 09:38:20 +0900

    refactor: 静的解説のエラー内容を解決

--- Algorithm/MeikaiPython/Chapter2/src/chapter2_2.py
+++ Algorithm/MeikaiPython/Chapter2/src/chapter2_2.py
@@ -4,18 +4,18 @@ from typing import Any, Sequence
 def max():
     print("配列の最大値を求めます。")
     num = int(input("要素数:"))
-    x = [None] * num
+    x = []
 
     for i in range(num):
-        x[i] = int(input(f"x[{i}]:"))
+        x.append(int(input(f"x[{i}]:")))
 
     print(f"最大値は{_max_of(x)}です。")
 
 
-def _max_of(a: Sequence) -> Any:
-    """シーケンスaの要素の最大値を返却する"""
-    maximum = a[0]
-    for i in range(1, len(a)):
-        if a[i] > maximum:
-            maximum = a[i]
+def _max_of(datas: Sequence) -> Any:
+    """シーケンスdatasの要素の最大値を返却する"""
+    maximum = datas[0]
+    for data in datas:
+        if data > maximum:
+            maximum = data
     return maximum
