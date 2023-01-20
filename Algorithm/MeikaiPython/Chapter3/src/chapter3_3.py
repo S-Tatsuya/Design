e6e7c1c465d8ce65b12fd05496a078253b578618
S.tatsuya <s.tatsuya0123@gmail.com>
2023-01-20 21:22:15 +0900

    add: 2分探索の振る舞いを表示する関数を実装

--- Algorithm/MeikaiPython/Chapter3/src/chapter3_3.py
+++ Algorithm/MeikaiPython/Chapter3/src/chapter3_3.py
@@ -42,3 +42,45 @@ def bsearch():
         print("その値の要素は存在しません。")
     else:
         print(f"その値はx[{idx}]にあります。")
+
+
+def bsearch_verbose(a: Sequence, key: Any) -> int:
+    pl = 0
+    pr = len(a) - 1
+
+    print("   |", end="")
+    for i in range(len(a)):
+        print(f"{i:4}", end="")
+    print("")
+    print("---+" + (4 * len(a) + 2) * "-")
+
+    while True:
+        pc = (pl + pr) // 2
+
+        print("   |", end="")
+        if pl != pc:
+            print((pl * 4 + 1) * " " + "<-" + ((pc - pl) * 4) * " " + "+", end="")
+        else:
+            print((pc * 4 + 1) * " " + "<+", end="")
+
+        if pc != pr:
+            print(((pr - pc) * 4 - 2) * " " + "->")
+        else:
+            print("->")
+        print(f"{pc:3}|", end="")
+
+        for i in range(len(a)):
+            print(f"{a[i]:4}", end="")
+        print("\n   |")
+
+        if a[pc] == key:
+            return pc
+        elif a[pc] < key:
+            pl = pc + 1
+        else:
+            pr = pc - 1
+
+        if pl > pr:
+            break
+
+    return -1
--- Algorithm/MeikaiPython/Chapter3/tests/test_chapter3_3.py
+++ Algorithm/MeikaiPython/Chapter3/tests/test_chapter3_3.py
@@ -20,3 +20,26 @@ class TestChapter3_3:
             "要素数:昇順に入力してください。\nx[0]:x[1]:x[2]:x[3]:x[4]:x[5]:x[6]:探す値:その値の要素は存在しません。\n"
         )
         assert err == ""
+
+    def test_bsearch_verbose(self, capfd):
+        datas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        result = sut.bsearch_verbose(datas, 8)
+        out, err = capfd.readouterr()
+        assert result == 7
+        assert out == (
+            "   |   0   1   2   3   4   5   6   7   8   9  10\n"
+            "---+----------------------------------------------\n"
+            "   | <-                    +                  ->\n"
+            "  5|   1   2   3   4   5   6   7   8   9  10  11\n"
+            "   |\n"
+            "   |                         <-        +      ->\n"
+            "  8|   1   2   3   4   5   6   7   8   9  10  11\n"
+            "   |\n"
+            "   |                         <+  ->\n"
+            "  6|   1   2   3   4   5   6   7   8   9  10  11\n"
+            "   |\n"
+            "   |                             <+->\n"
+            "  7|   1   2   3   4   5   6   7   8   9  10  11\n"
+            "   |\n"
+        )
+        assert err == ""
