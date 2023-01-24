9faa837901f82c1320f8d5fc21c8e108d66473a3
S.tatsuya <s.tatsuya0123@gmail.com>
2023-01-24 20:47:48 +0900

    add: push, pop

--- Algorithm/MeikaiPython/Chapter4/src/chapter4_1.py
+++ Algorithm/MeikaiPython/Chapter4/src/chapter4_1.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class FixedStack:
     class Empty(Exception):
         pass
@@ -8,7 +11,27 @@ class FixedStack:
     def __init__(self, capacity: int = 256):
         self.stk = [None] * capacity
         self.capacity = capacity
-        self.prt = 0
+        self.ptr = 0
 
     def __len__(self) -> int:
-        return self.prt
+        return self.ptr
+
+    def push(self, value: Any):
+        if self.is_full():
+            raise FixedStack.Full
+
+        self.stk[self.ptr] = value
+        self.ptr += 1
+
+    def pop(self) -> Any:
+        if self.is_empty():
+            raise FixedStack.Empty
+
+        self.ptr -= 1
+        return self.stk[self.ptr]
+
+    def is_full(self) -> bool:
+        return self.ptr >= self.capacity
+
+    def is_empty(self) -> bool:
+        return self.ptr <= 0
--- Algorithm/MeikaiPython/Chapter4/tests/test_chapter4_1.py
+++ Algorithm/MeikaiPython/Chapter4/tests/test_chapter4_1.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.chapter4_1 import FixedStack
 
 
@@ -6,3 +8,37 @@ class TestChapter4_1:
         sut = FixedStack()
         assert sut.__len__() == 0
         assert len(sut) == 0
+
+        sut.push(1)
+
+        assert sut.__len__() == 1
+        assert len(sut) == 1
+
+        result = sut.pop()
+
+        assert result == 1
+        assert sut.__len__() == 0
+        assert len(sut) == 0
+
+    def test_full_exception(self):
+        sut = FixedStack(2)
+
+        assert sut.is_full() is False
+        assert sut.is_empty()
+        sut.push(1)
+        sut.push(2)
+        assert sut.is_full()
+        assert sut.is_empty() is False
+
+        with pytest.raises(FixedStack.Full) as e:
+            sut.push(3)
+
+        assert str(e.value) == ""
+
+    def test_empty_exception(self):
+        sut = FixedStack()
+        assert sut.is_empty()
+        with pytest.raises(FixedStack.Empty) as e:
+            _ = sut.pop()
+
+        assert str(e.value) == ""
