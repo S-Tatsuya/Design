fe9905fea16d60c053f868a6cd9ba3e5d1d3ff65
S.tatsuya <s.tatsuya0123@gmail.com>
2023-01-25 18:16:05 +0900

    add: enque

--- Algorithm/MeikaiPython/Chapter4/src/chapter4_2.py
+++ Algorithm/MeikaiPython/Chapter4/src/chapter4_2.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class FixedQueue:
     class Empty(Exception):
         pass
@@ -5,7 +8,7 @@ class FixedQueue:
     class Full(Exception):
         pass
 
-    def __init__(self, capacity: int):
+    def __init__(self, capacity: int = 10):
         self.no = 0
         self.front = 0
         self.rear = 0
@@ -20,3 +23,14 @@ class FixedQueue:
 
     def is_full(self) -> bool:
         return self.no >= self.capacity
+
+    def enque(self, x: Any):
+        if self.is_full():
+            raise FixedQueue.Full
+
+        self.que[self.rear] = x
+        self.rear += 1
+        self.no += 1
+
+        if self.rear == self.capacity:
+            self.rear = 0
--- Algorithm/MeikaiPython/Chapter4/tests/test_chapter4_2.py
+++ Algorithm/MeikaiPython/Chapter4/tests/test_chapter4_2.py
@@ -1,9 +1,26 @@
+import pytest
+
 from src.chapter4_2 import FixedQueue
 
 
 class TestChapter4_2:
     def test_fixed_queue(self):
-        sut = FixedQueue(10)
+        sut = FixedQueue()
         assert sut.is_empty()
         assert sut.is_full() is False
         assert sut.__len__() == 0
+
+        sut.enque(1)
+
+        assert sut.is_empty() is False
+        assert sut.is_full() is False
+        assert sut.__len__() == 1
+
+    def test_full_expection(self):
+        sut = FixedQueue(2)
+        sut.enque(1)
+        sut.enque(2)
+        with pytest.raises(FixedQueue.Full) as e:
+            sut.enque(3)
+
+        assert str(e.value) == ""
