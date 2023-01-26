29241e757e2c9c0694599d8a3e1314deef1927be
S.tatsuya <s.tatsuya0123@gmail.com>
2023-01-26 18:10:42 +0900

    add: 例外を出すコードを追加

--- Algorithm/MeikaiPython/Chapter5/src/chapter5_1.py
+++ Algorithm/MeikaiPython/Chapter5/src/chapter5_1.py
@@ -1,4 +1,7 @@
 def factorial(value: int) -> int:
+    if value < 0:
+        raise ValueError("負の数は受け付けません")
+
     if value <= 0:
         return 1
 
--- Algorithm/MeikaiPython/Chapter5/tests/test_chapter5_1.py
+++ Algorithm/MeikaiPython/Chapter5/tests/test_chapter5_1.py
@@ -1,6 +1,14 @@
+import pytest
+
 import src.chapter5_1 as sut
 
 
 class TestChapter5_1:
     def test_factorial(self):
         assert sut.factorial(3) == 6
+
+    def test_factorial_exception_value_error(self):
+        with pytest.raises(ValueError) as e:
+            sut.factorial(-1)
+
+        assert str(e.value) == "負の数は受け付けません"
