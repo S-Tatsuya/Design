91ddf71b9b9cc3adc07d099fcad957ab5a89a1cf
S.tatsuya <s.tatsuya0123@gmail.com>
2023-01-27 18:38:09 +0900

    refactor: 不要な交換ソートの実行を停止

--- Algorithm/MeikaiPython/Chapter6/src/chapter6_2.py
+++ Algorithm/MeikaiPython/Chapter6/src/chapter6_2.py
@@ -4,9 +4,13 @@ from typing import MutableSequence
 def bubble_sort1(target: list[int]) -> list[int]:
     result = target
     for i in range(len(result) - 1):
+        exchng = 0
         for j in reversed(range(i + 1, len(result))):
             if result[j] < result[j - 1]:
                 result[j - 1], result[j] = result[j], result[j - 1]
+                exchng += 1
+        if exchng == 0:
+            break
 
     return result
 
