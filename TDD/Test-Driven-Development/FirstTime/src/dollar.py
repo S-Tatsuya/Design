e6d3231df63b95a61db5c55177e9647d71e4de64
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-02 21:27:59 +0900

    Green: メンバー変数のamountをMoneyクラスに移動

--- TDD/Test-Driven-Development/FirstTime/src/dollar.py
+++ TDD/Test-Driven-Development/FirstTime/src/dollar.py
@@ -5,9 +5,6 @@ from src.money import Money
 
 
 class Dollar(Money):
-    def __init__(self, amount):
-        self._amount = amount
-
     def times(self, multiplier: int) -> Dollar:
         return Dollar(self._amount * multiplier)
 
