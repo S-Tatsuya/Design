3ef21b8507ada94439d83c0dc1595b54c874585e
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-02 20:25:39 +0900

    Green: Dollarの副作用を取り除くことに成功

--- TDD/Test-Driven-Development/FirstTime/src/dollar.py
+++ TDD/Test-Driven-Development/FirstTime/src/dollar.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
+
 class Dollar:
     def __init__(self, amount):
         self.amount = amount
 
-    def times(self, multiplier: int):
-        self.amount = self.amount * multiplier
+    def times(self, multiplier: int) -> Dollar:
+        return Dollar(self.amount * multiplier)
