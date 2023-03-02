60761b8482e507458ce391c1a7836fdd581839b8
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-02 21:08:48 +0900

    Green: 掛け算のテストをより意図が伝わるテストに修正

--- TDD/Test-Driven-Development/FirstTime/tests/test_money.py
+++ TDD/Test-Driven-Development/FirstTime/tests/test_money.py
@@ -4,10 +4,8 @@ from src.dollar import Dollar
 class TestMoney:
     def test_multiplication(self):
         five = Dollar(5)
-        product = five.times(2)
-        assert product.amount == 10
-        product = five.times(3)
-        assert product.amount == 15
+        assert Dollar(10).equals(five.times(2))
+        assert Dollar(15).equals(five.times(3))
 
     def test_equality(self):
         assert Dollar(5).equals(Dollar(5))
