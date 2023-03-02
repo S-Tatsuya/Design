f1ae01d8c8f1705a6fced117c5d50c638b0028a0
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-02 21:36:52 +0900

    Green: FrancクラスからMoneyクラスと重複する要素を削除
    メンバー変数: amount
    メンバーメソッド: equals

--- TDD/Test-Driven-Development/FirstTime/src/franc.py
+++ TDD/Test-Driven-Development/FirstTime/src/franc.py
@@ -1,17 +1,8 @@
 from __future__ import annotations
-from typing import Any
 
 from src.money import Money
 
 
 class Franc(Money):
-    def __init__(self, amount):
-        self._amount = amount
-
     def times(self, multiplier: int) -> Franc:
         return Franc(self._amount * multiplier)
-
-    def equals(self, object: Any):
-        if hasattr(object, "_amount"):
-            return self._amount == object._amount
-        return False
