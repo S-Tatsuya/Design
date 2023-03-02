0642a6bbecbcb5961b8f5294b450cb4cf2c1e846
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-02 21:19:13 +0900

    Red: Francクラスのmultiplicationメソッドのテストを追加

--- TDD/Test-Driven-Development/FirstTime/tests/test_money.py
+++ TDD/Test-Driven-Development/FirstTime/tests/test_money.py
@@ -10,3 +10,8 @@ class TestMoney:
     def test_equality(self):
         assert Dollar(5).equals(Dollar(5))
         assert not Dollar(5).equals(Dollar(6))
+
+    def test_franc_multiplication(self):
+        five = Franc(5)
+        assert Franc(10).equals(five.times(2))
+        assert Franc(15).equals(five.times(3))
