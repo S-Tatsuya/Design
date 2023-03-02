a991799b1a9a73a20e34d800fed269bbe9466eb5
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-02 19:04:01 +0900

    Red: ビルドが通る状態になった

---
+++ TDD/Test-Driven-Development/FirstTime/src/dollar.py
@@ -0,0 +1,6 @@
+class Dollar:
+    def __init__(self, amount):
+        self.amount = 0
+
+    def times(self, multiplier: int):
+        pass
--- TDD/Test-Driven-Development/FirstTime/tests/test_money.py
+++ TDD/Test-Driven-Development/FirstTime/tests/test_money.py
@@ -1,5 +1,8 @@
+from src.dollar import Dollar
+
+
 class TestMoney:
     def test_multiplication(self):
         five = Dollar(5)
         five.times(2)
-        Assert five.amount == 10
+        assert five.amount == 10
