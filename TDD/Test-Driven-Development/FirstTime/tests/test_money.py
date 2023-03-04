38f3757c16c58efdc25433f533c9adcd3eacb27d
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-04 16:51:17 +0900

    Green: 不要なテストケースを削除
    DollarクラスとFrancクラスがMoneyクラスになったため、FrancクラスとDollarクラスでそれぞれ行っていたテストは一つに集約しても問題無い。
    また、異なるクラス間の計算についてもMoneyクラスに集約してクラスを分けないことにしたので、設計上不要なテストであると判断して削除する。

--- TDD/Test-Driven-Development/FirstTime/tests/test_money.py
+++ TDD/Test-Driven-Development/FirstTime/tests/test_money.py
@@ -1,4 +1,3 @@
-from src.money import Money
 from src.money_factory import MoneyFactory
 
 
@@ -11,18 +10,8 @@ class TestMoney:
     def test_equality(self):
         assert MoneyFactory.dollar(5).equals(MoneyFactory.dollar(5))
         assert not MoneyFactory.dollar(5).equals(MoneyFactory.dollar(6))
-        assert MoneyFactory.franc(5).equals(MoneyFactory.franc(5))
-        assert not MoneyFactory.franc(5).equals(MoneyFactory.franc(6))
         assert not MoneyFactory.franc(5).equals(MoneyFactory.dollar(5))
-
-    def test_franc_multiplication(self):
-        five = MoneyFactory.franc(5)
-        assert MoneyFactory.franc(10).equals(five.times(2))
-        assert MoneyFactory.franc(15).equals(five.times(3))
 
     def test_currency(self):
         assert MoneyFactory.dollar(1).currency() == "USD"
         assert MoneyFactory.franc(1).currency() == "CHF"
-
-    def test_different_class_equality(self):
-        assert Money(10, "CHF").equals(MoneyFactory.franc(10))
