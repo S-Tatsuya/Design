5828de7b6cb64e264c0521b7b147f389f4bb6696
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-04 13:31:51 +0900

    Green: Moneyクラスを抽象クラスにしてtimesメソッドを抽象メソッドとして実装

--- TDD/Test-Driven-Development/FirstTime/src/money.py
+++ TDD/Test-Driven-Development/FirstTime/src/money.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
+from abc import ABCMeta, abstractmethod
 from typing import Any
 
 
-class Money:
+class Money(metaclass=ABCMeta):
     def __init__(self, amount):
         self._amount = amount
 
@@ -9,3 +11,7 @@ class Money:
         if hasattr(object, "_amount"):
             return self._amount == object._amount and type(self) == type(object)
         return False
+
+    @abstractmethod
+    def times(self, multipliner: int) -> Money:
+        pass
