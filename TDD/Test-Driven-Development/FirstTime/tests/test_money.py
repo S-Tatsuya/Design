bfe0e4b9984a530a81672445b6572b8dcf885271
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-04 13:35:45 +0900

    Green: テストコードからMoneyクラスのサブクラスDollarクラスを削除

--- TDD/Test-Driven-Development/FirstTime/tests/test_money.py
+++ TDD/Test-Driven-Development/FirstTime/tests/test_money.py
@@ -1,20 +1,19 @@
 from src.money_factory import MoneyFactory
-from src.dollar import Dollar
 from src.franc import Franc
 
 
 class TestMoney:
     def test_multiplication(self):
         five = MoneyFactory.dollar(5)
-        assert Dollar(10).equals(five.times(2))
-        assert Dollar(15).equals(five.times(3))
+        assert MoneyFactory.dollar(10).equals(five.times(2))
+        assert MoneyFactory.dollar(15).equals(five.times(3))
 
     def test_equality(self):
-        assert Dollar(5).equals(Dollar(5))
-        assert not Dollar(5).equals(Dollar(6))
+        assert MoneyFactory.dollar(5).equals(MoneyFactory.dollar(5))
+        assert not MoneyFactory.dollar(5).equals(MoneyFactory.dollar(6))
         assert Franc(5).equals(Franc(5))
         assert not Franc(5).equals(Franc(6))
-        assert not Franc(5).equals(Dollar(5))
+        assert not Franc(5).equals(MoneyFactory.dollar(5))
 
     def test_franc_multiplication(self):
         five = Franc(5)
