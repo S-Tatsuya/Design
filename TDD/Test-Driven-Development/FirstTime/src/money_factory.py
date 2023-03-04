8e7c0be792ed19003732c7e19f0a3d9aee1910d1
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-04 15:24:45 +0900

    Green: FrancクラスをMoneFactoryで生成するように変更

--- TDD/Test-Driven-Development/FirstTime/src/money_factory.py
+++ TDD/Test-Driven-Development/FirstTime/src/money_factory.py
@@ -1,7 +1,12 @@
 from src.dollar import Dollar
+from src.franc import Franc
 
 
 class MoneyFactory:
     @staticmethod
     def dollar(amount):
         return Dollar(amount)
+
+    @staticmethod
+    def franc(amount):
+        return Franc(amount)
