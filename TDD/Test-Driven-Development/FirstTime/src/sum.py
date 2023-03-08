1b356f755634e56084408e94e5b2e745a96ff1bb
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-08 21:23:19 +0900

    Green: Sumのメンバー変数をExpression型に変更した

--- TDD/Test-Driven-Development/FirstTime/src/sum.py
+++ TDD/Test-Driven-Development/FirstTime/src/sum.py
@@ -3,9 +3,9 @@ import src.money_factory as MoneyFactory
 
 
 class Sum(Expression):
-    def __init__(self, augend, addend):
-        self.augend = augend
-        self.addend = addend
+    def __init__(self, augend: Expression, addend: Expression):
+        self.augend: Expression = augend
+        self.addend: Expression = addend
 
     def reduce(self, bank, to):
         amount = (
