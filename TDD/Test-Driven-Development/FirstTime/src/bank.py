48cc07c2e8e4d42587cd3c803b208987f5ff925f
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-08 18:52:22 +0900

    Green: reduceメソッドをSumクラスに移した
    SumクラスとMoneyクラスに循環参照が発生してしまったので、importをfromではなくimport文を使って実施している。

--- TDD/Test-Driven-Development/FirstTime/src/bank.py
+++ TDD/Test-Driven-Development/FirstTime/src/bank.py
@@ -1,8 +1,4 @@
-from src.money_factory import MoneyFactory
-
-
 class Bank:
     def reduce(self, source, to):
         sum = source
-        amount = sum.augend.amount + sum.addend.amount
-        return MoneyFactory.money(amount, to)
+        return sum.reduce(to)
--- TDD/Test-Driven-Development/FirstTime/src/sum.py
+++ TDD/Test-Driven-Development/FirstTime/src/sum.py
@@ -1,7 +1,12 @@
 from src.expression import Expression
+import src.money_factory as MoneyFactory
 
 
 class Sum(Expression):
     def __init__(self, augend, addend):
         self.augend = augend
         self.addend = addend
+
+    def reduce(self, to):
+        amount = self.augend.amount + self.addend.amount
+        return MoneyFactory.MoneyFactory.money(amount, to)
