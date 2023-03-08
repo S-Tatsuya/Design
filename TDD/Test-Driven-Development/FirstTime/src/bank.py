fe78609847ce3931bdc33168abb94e3922758d62
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-08 19:01:42 +0900

    Green: テストを通すためのコードを書いた

--- TDD/Test-Driven-Development/FirstTime/src/bank.py
+++ TDD/Test-Driven-Development/FirstTime/src/bank.py
@@ -1,4 +1,9 @@
+from src.money import Money
+
+
 class Bank:
     def reduce(self, source, to):
+        if isinstance(source, Money):
+            return source
         sum = source
         return sum.reduce(to)
