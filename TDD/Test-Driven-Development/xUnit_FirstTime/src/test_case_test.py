f1023b947d64ee293148fff21b8382a629fd6c53
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-11 10:50:27 +0900

    Green: Test caseを実行するクラスを作成した
    テストを実行した場合に成功すれば何も出ない。
    失敗した場合にassertエラーが出力される。

---
+++ TDD/Test-Driven-Development/xUnit_FirstTime/src/test_case_test.py
@@ -0,0 +1,13 @@
+from test_case import TestCase
+from wasrun import WasRun
+
+
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert not test.wasRun
+        test.run()
+        assert test.wasRun
+
+
+TestCaseTest("testRunning").run()
