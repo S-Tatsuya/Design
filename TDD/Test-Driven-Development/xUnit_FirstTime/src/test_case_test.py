54718764bef235b8bffaeb568ed3f23b33b265f3
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-14 21:22:45 +0900

    Green: 実装のミスを発見したので修正

--- TDD/Test-Driven-Development/xUnit_FirstTime/src/test_case_test.py
+++ TDD/Test-Driven-Development/xUnit_FirstTime/src/test_case_test.py
@@ -4,9 +4,9 @@ from wasrun import WasRun
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
-        self.test = WasRun("testMethod")
-        self.test.run()
-        assert "setUp testMethod tearDown " == self.test.log
+        test = WasRun("testMethod")
+        test.run()
+        assert "setUp testMethod tearDown " == test.log
 
 
 TestCaseTest("testTemplateMethod").run()
