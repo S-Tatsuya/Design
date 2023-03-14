95b9beab4a3b386229b3e8354294fa06e1a38398
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-14 20:46:00 +0900

    Green: リファクタリングを実施した

--- TDD/Test-Driven-Development/xUnit_FirstTime/src/test_case_test.py
+++ TDD/Test-Driven-Development/xUnit_FirstTime/src/test_case_test.py
@@ -5,7 +5,6 @@ from wasrun import WasRun
 class TestCaseTest(TestCase):
     def testRunning(self):
         test = WasRun("testMethod")
-        assert not test.wasRun
         test.run()
         assert test.wasRun
 
--- TDD/Test-Driven-Development/xUnit_FirstTime/src/wasrun.py
+++ TDD/Test-Driven-Development/xUnit_FirstTime/src/wasrun.py
@@ -2,10 +2,6 @@ from test_case import TestCase
 
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
-
     def setUp(self):
         self.wasRun = None
         self.wasSetUp = 1
