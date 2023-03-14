fb0f1916f0c6f45d98cdac00ab50e28208492798
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-14 21:13:22 +0900

    Green: setUpメソッドが不要になったので削除

--- TDD/Test-Driven-Development/xUnit_FirstTime/src/test_case_test.py
+++ TDD/Test-Driven-Development/xUnit_FirstTime/src/test_case_test.py
@@ -3,10 +3,8 @@ from wasrun import WasRun
 
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
     def testTemplateMethod(self):
+        self.test = WasRun("testMethod")
         self.test.run()
         assert "setUp testMethod " == self.test.log
 
