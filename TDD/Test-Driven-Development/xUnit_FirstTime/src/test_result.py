1e84eb1011e1a1f97583c03ef6f4f543fc5951f9
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-14 21:30:57 +0900

    Green: リファクタリング テスト実行回数をカウントするように修正

--- TDD/Test-Driven-Development/xUnit_FirstTime/src/test_result.py
+++ TDD/Test-Driven-Development/xUnit_FirstTime/src/test_result.py
@@ -1,6 +1,9 @@
 class TestResult:
     def __init__(self):
-        self.runCount = 1
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
 
     def summary(self):
         return f"{self.runCount} run, 0 failed"
