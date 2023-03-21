f0813dd3565a990c7ab6a345fd5998843630d7a2
S.tatsuya <s.tatsuya0123@gmail.com>
2023-03-21 16:23:49 +0900

    add: 複雑なデータ構造でのwheelの実装

--- ObjectRuby/Second/gear.py
+++ ObjectRuby/Second/gear.py
@@ -30,8 +30,25 @@ class Gear:
         return self.ratio * (self.rim + (self.tire * 2))
 
 
+class ObscuringReferences:
+    def __init__(self, data):
+        self._data = data
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def diameters(self):
+        return [(x[0] + (x[1] * 2)) for x in self.data]
+
+
 if __name__ == "__main__":
     print(Gear(52, 11, 26, 1.5).ratio)
     print(Gear(30, 27, 24, 1.25).ratio)
     print(Gear(52, 11, 26, 1.5).gear_inches)
     print(Gear(30, 27, 24, 1.25).gear_inches)
+
+    object = ObscuringReferences([[622, 20], [622, 23], [559, 30], [559, 40]])
+    for diameters in object.diameters:
+        print(diameters)
